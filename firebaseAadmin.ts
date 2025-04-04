e212038e8d5dbb43c12fa8c9adcf1947f3e0791d
Shahreen <shahreen.husne.rabbani@gmail.com>
2025-04-04 16:00:24 +0600

    firebase admin initialization

---
+++ firebaseAadmin.ts
@@ -0,0 +1,15 @@
+import { getApps } from "firebase/app";
+import admin from "firebase-admin"
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string);
+
+// it mean getApps length is 0 then we will enter the if block 
+if (!getApps.length){
+
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount)
+    });
+}
+
+const adminDB = admin.firestore();
+export {adminDB}
